a28b7d7e5246eff609ca6139851e241fe58c61a1
Sundarm Soni <sundarm.soni097@gmail.com>
2020-02-20 11:05:48 +0530

    Adding Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,18 +7,20 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './services/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './AuthGuard/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent, children: [
+  { path: 'recipes', component: RecipesComponent,canActivate:[AuthGuard], children: [
     { path: '', component: RecipeStartComponent },
     { path: 'new', component: RecipeEditComponent},
     { path: ':id', component: RecipeDetailComponent, resolve: {RecipesResolverService} },
     { path: ':id/edit', component: RecipeEditComponent, resolve: {RecipesResolverService} }
 
   ] },
-  { path: 'shoppinglist', component: ShoppingListComponent },
-  { path:'auth', component: AuthComponent }
+  { path: 'shoppinglist', component: ShoppingListComponent, canActivate:[AuthGuard] },
+  { path:'auth', component: AuthComponent },
+  { path: '**', component: AuthComponent }
 
 ];
 
